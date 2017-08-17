2ea0e4050114cdf42bc1af54925141ef391b45a8
kangchonsara <kckim@uchicago.edu>
2017-08-17 11:23:25 -0500

    subs by sites according to shih et al epitopes

--- script/nonsyn_subs.py
+++ script/nonsyn_subs.py
@@ -66,7 +66,7 @@ def find_substitutions(seqs_by_season):
 			if ancestrals_each_site[s][0] != ancestrals_by_season[season-1][s][0]:
 				subs[season].append((ancestrals_by_season[season-1][s][0], s+1, ancestrals_each_site[s][0]))
 
-	return subs
+	return subs, ancestrals_by_season
 
 def put_seqs_by_season_North(infName, beginS, endS):
 	inf = open(infName, "rU")
@@ -124,8 +124,19 @@ def put_seqs_by_year(infName, beginY, endY):
 	inf.close()
 	return seqs_by_year
 
+def read_epitopes(epifName):
+	epif = open(epifName, "rU")
+	epi_dict = dict()
+	for line in epif:
+		each = line.split("\n")[0].split("\t")
+		epi_dict[int(each[0])] = each[1]
+	return epi_dict
+		
+
 dataDir = sys.argv[1]
 resultDir = sys.argv[2]
+rootYear = 1968
+
 #from 1968 to 1992, put sequences by year
 seqs_by_year = put_seqs_by_year(dataDir+"ncbi_aligned_6812_AA.fas", 1968, 1992)
 
@@ -137,8 +148,68 @@ seqs_by_season_North_2 = put_seqs_by_season_North(dataDir+"gisaid_aligned_1217_A
 
 seqs_by_season = seqs_by_year + seqs_by_season_North_1 + seqs_by_season_North_2
 
-subs = find_substitutions(seqs_by_season)
+subs, ancestrals_by_season = find_substitutions(seqs_by_season)
+
+shih_epi_dict = read_epitopes(dataDir+"shih_epitope.txt")
+
+epitope_sites = ['a', 'b', 'c', 'd', 'e', 'o', 'HA2']
 
 outf = open(resultDir+"substitutions.txt", "w")
-for season in range(len(subs)):
-	print (subs[season])
+
+for sdx in range(len(subs)):
+	subs_by_sites = [[] for sites in range(len(epitope_sites))]	
+	season = str(sdx+rootYear)
+	outf.write(season + "\n")
+	
+	for sub in subs[sdx]:
+		#determine the site this substitution belong to and save in a list "subs_by_sites"
+		try:
+			#this substitution occured at one of epitoep sites a, b, c, d, e
+			episite = shih_epi_dict[sub[1]]
+			subs_by_sites[epitope_sites.index(episite)].append(sub)
+		except KeyError:
+			if sub[1] > 329:
+				episite = 'HA2'
+				subs_by_sites[epitope_sites.index(episite)].append(sub)
+			else:
+				episite = 'o'
+				subs_by_sites[epitope_sites.index(episite)].append(sub)
+	
+	longest_len = len(subs_by_sites[0])
+	for i in range(len(subs_by_sites)):
+		if len(subs_by_sites[i]) > longest_len:
+			longest_len = len(subs_by_sites[i])
+
+	for i in range(longest_len):
+		oneline = "	,"
+		for site in subs_by_sites:
+			try:
+				oneline += site[i][0] + str(site[i][1]) + site[i][2] + ","
+			except IndexError:
+				oneline += ' ,'
+		outf.write(oneline+"\n")
+	
+	
+
+outf.close()
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	
+
+
+
+
+
+	
+	
